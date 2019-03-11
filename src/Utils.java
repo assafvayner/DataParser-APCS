271a115f0250037559803cb0af020998f4a41ff8
Assaf Vayner <assafvayner@gmail.com>
2019-03-11 17:28:17 +0100

    removed old methods

--- src/Utils.java
+++ src/Utils.java
@@ -133,38 +133,38 @@ public class Utils {
         }
     }
 
-    public static ArrayList<ElectionResult> parse2016PresidentialResults() {
-        ArrayList<ElectionResult> results = new ArrayList<>();
-
-        String dataFromCSV = readFileAsString("data/2016_Presidential_Results.csv");
-
-        String[] lines = dataFromCSV.split("\n");
-
-        String auxStr;
-        String[] lineArr;
-        for (int i = 1; i < lines.length; i++) {
-            auxStr = removeBadChars(lines[i]);
-            lineArr = auxStr.split(",");
-            results.add(makeElectionResultObj(lineArr));
-        }
-        return results;
-    }
-
-    private static ElectionResult makeElectionResultObj(String[] lineArr) {
-        int votes_dem = (int) Double.parseDouble(lineArr[1]);
-        int votes_gop = (int) Double.parseDouble(lineArr[2]);
-        int total_votes = (int) Double.parseDouble(lineArr[3]);
-        double per_dem = Double.parseDouble(lineArr[4]) / 100;
-        double per_gop = Double.parseDouble(lineArr[5]) / 100;
-        int diff = Integer.parseInt(lineArr[6]);
-        double per_point_diff = Double.parseDouble(lineArr[7]) / 100;
-        ;
-        String state_abbr = lineArr[8];
-        String county_name = lineArr[9];
-        int combined_fips = Integer.parseInt(lineArr[10]);
-
-        return new ElectionResult(votes_dem, votes_gop, total_votes, per_dem, per_gop,
-                diff, per_point_diff, state_abbr, county_name, combined_fips);
-    }
+//    public static ArrayList<ElectionResult> parse2016PresidentialResults() {
+//        ArrayList<ElectionResult> results = new ArrayList<>();
+//
+//        String dataFromCSV = readFileAsString("data/2016_Presidential_Results.csv");
+//
+//        String[] lines = dataFromCSV.split("\n");
+//
+//        String auxStr;
+//        String[] lineArr;
+//        for (int i = 1; i < lines.length; i++) {
+//            auxStr = removeBadChars(lines[i]);
+//            lineArr = auxStr.split(",");
+//            results.add(makeElectionResultObj(lineArr));
+//        }
+//        return results;
+//    }
+//
+//    private static ElectionResult makeElectionResultObj(String[] lineArr) {
+//        int votes_dem = (int) Double.parseDouble(lineArr[1]);
+//        int votes_gop = (int) Double.parseDouble(lineArr[2]);
+//        int total_votes = (int) Double.parseDouble(lineArr[3]);
+//        double per_dem = Double.parseDouble(lineArr[4]) / 100;
+//        double per_gop = Double.parseDouble(lineArr[5]) / 100;
+//        int diff = Integer.parseInt(lineArr[6]);
+//        double per_point_diff = Double.parseDouble(lineArr[7]) / 100;
+//        ;
+//        String state_abbr = lineArr[8];
+//        String county_name = lineArr[9];
+//        int combined_fips = Integer.parseInt(lineArr[10]);
+//
+//        return new ElectionResult(votes_dem, votes_gop, total_votes, per_dem, per_gop,
+//                diff, per_point_diff, state_abbr, county_name, combined_fips);
+//    }
 
 }
